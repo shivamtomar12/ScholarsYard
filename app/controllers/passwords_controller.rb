af3808fb35a1bcf108e4f109677478e0edf0ceab
sstomar <shivamsingh-tomar@uiowa.edu>
2014-11-11 15:40:10 -0400

    Admin Added

---
+++ app/controllers/passwords_controller.rb
@@ -0,0 +1,23 @@
+class PasswordsController < Devise::PasswordsController
+   def new
+     super
+
+   end
+
+   def create
+     
+    super
+   end
+   
+   def edit
+     super
+   end
+   
+   def update
+     super
+   end
+  
+end
+
+
+
