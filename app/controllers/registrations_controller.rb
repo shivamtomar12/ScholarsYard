ad29a6380703b2b0341978d2943d0aca6411d120
sstomar <shivamsingh-tomar@uiowa.edu>
2014-11-28 21:49:57 -0400

    added certificate pdf download

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,12 +5,15 @@ class RegistrationsController < Devise::RegistrationsController
 
   def create
      super
-    
+    if current_user==nil
+     flash[:notice] = "Please enter correct credentials"
+    else
      hash=User.find_by_email(current_user.email)
      hash.user_type="user"
      session[:user_type]=hash.user_type
      hash.save
      puts "current user #{session[:user_type]}"
+    end
      
      
    end
